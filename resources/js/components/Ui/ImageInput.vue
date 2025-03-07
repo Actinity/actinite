689d118084de1e7ad7b6c9d91e10e7d010fd0937
Kieran Ashley <kieran@actinity.com>
2025-03-07 13:46:32 +0000

    Allow deleting images

--- resources/js/components/Ui/ImageInput.vue
+++ resources/js/components/Ui/ImageInput.vue
@@ -1,8 +1,18 @@
 <template>
 <div>
-    <div @click="edit" class="d-inline-block" style="cursor: pointer;">
+    <div @click="edit" class="d-inline-block position-relative image-input" style="cursor: pointer;">
         <img :src="localPath" v-if="localValue" class="ac-editor-image" />
-        <div v-else>Click to browse</div>
+        <button v-else class="btn btn-outline-light">Click to browse</button>
+
+
+	    <div
+		    v-if="this.localValue"
+		    @click.stop="trash"
+            class="image-trash"
+	    >
+		        <i class="fas fa-trash"></i>
+	    </div>
+
     </div>
 
 </div>
@@ -23,7 +33,10 @@ export default {
         },
         edited(asset) {
             this.localValue = asset ? 'cloudinary://'+asset.uuid : null;
-        }
+        },
+	    trash() {
+			this.localValue = null;
+	    }
     },
     computed: {
 		isCustomUrl() {
@@ -47,3 +60,22 @@ export default {
     }
 }
 </script>
+<style>
+.image-trash {
+	background-color: rgba(255,255,255,0.9);
+	padding: 10px 15px;
+	top: 0;
+	right: 0;
+	position: absolute;
+	pointer-events: none;
+	transition: 0.15s opacity ease-out;
+	opacity: 0;
+}
+.image-input:hover .image-trash {
+	opacity: 1;
+	pointer-events: auto;
+}
+.image-trash:hover {
+	color: #f00;
+}
+</style>
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,7 @@ const mix = require('laravel-mix');
 mix.setPublicPath('./dist')
 
 mix.js('resources/js/app.js', 'dist').vue()
-    .sass('resources/sass/app.scss', 'dist')
+    .sass('resources/sass/app.scss', 'dist',{
+	    implementation: require('sass')
+    })
     .version();
